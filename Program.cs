0ec0ceaaa3a005ea111de101b0220a75dbd162ed
vinicius <vinisurf1206@gmail.com>
2022-06-02 22:26:21 -0300

    tarefa concluida

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int idade;
+
+Console.Write("Digite sua idade: ");
+idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade > 18 && idade < 68)
+{
+
+    Console.WriteLine("Você pode ser doador.");
+
+}
+
+else
+{
+
+    Console.WriteLine("Você não pode ser doador.");
+
+}
